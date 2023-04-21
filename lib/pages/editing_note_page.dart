0211711e251055f3f7a263117804ea1c87ca475a
João Ribeiro <joao.rosado.ribeiro@gmail.com>
2023-04-21 17:42:19 +0100

    Add quill controller & loading a existing note

--- lib/pages/editing_note_page.dart
+++ lib/pages/editing_note_page.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_quill/flutter_quill.dart';
 import 'package:notes_app/models/note.dart';
 
 class EditingNotePage extends StatefulWidget {
@@ -12,6 +13,27 @@ class EditingNotePage extends StatefulWidget {
 }
 
 class _EditingNotePageState extends State<EditingNotePage> {
+  QuillController _controller = QuillController.basic();
+
+  @override
+  void initState() {
+    super.initState();
+    loadExistingNote();
+  }
+
+  //load existing note
+  void loadExistingNote() {
+    final doc = Document()..insert(0, widget.note.text);
+    setState(() {
+      _controller = QuillController(
+          document: doc, selection: const TextSelection.collapsed(offset: 0));
+    });
+  }
+
+  //TODO: add new note
+
+  //TODO: update existing note
+
   @override
   Widget build(BuildContext context) {
     return Scaffold();
