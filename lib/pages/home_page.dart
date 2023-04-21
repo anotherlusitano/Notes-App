4347a81b38bb299ffbfe2a5f927f01fc24c0ef60
João Ribeiro <joao.rosado.ribeiro@gmail.com>
2023-04-21 15:34:27 +0100

    Add heading to home page

--- lib/pages/home_page.dart
+++ lib/pages/home_page.dart
@@ -10,6 +10,22 @@ class HomePage extends StatefulWidget {
 class _HomePageState extends State<HomePage> {
   @override
   Widget build(BuildContext context) {
-    return const Scaffold();
+    return Scaffold(
+      appBar: AppBar(),
+      body: Column(
+        children: [
+          //heading
+          Padding(
+            padding: EdgeInsets.all(25.0),
+            child: Text(
+              'Notes',
+              style: TextStyle(fontSize: 32, fontWeight: FontWeight.bold),
+            ),
+          ),
+
+          //TODO: list of notes
+        ],
+      ),
+    );
   }
 }
